1b93896a97f3b5171c5167dbc41c3dc56302ea3b
lotus77888 <lotus77888@gmail.com>
2017-08-06 01:31:43 +0530

    Add Heroku creds and update default route to hit the DB.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
+
+// Host
+// ec2-54-243-107-66.compute-1.amazonaws.com
+// Database
+// d3rdu1uade4cgo
+// User
+// gblncdnpeyalmp
+// Port
+// 5432
+// Password
+// f6790882e739216710f6fd7aff3c7e85dbee83be7aac8fce87787f67085c8996
+// URI
+// postgres://gblncdnpeyalmp:f6790882e739216710f6fd7aff3c7e85dbee83be7aac8fce87787f67085c8996@ec2-54-243-107-66.compute-1.amazonaws.com:5432/d3rdu1uade4cgoHeroku CLI
+// heroku pg:psql postgresql-dimensional-49271 --app clickersone
+
+
+
+// postgres://gblncdnpeyalmp:f6790882e739216710f6fd7aff3c7e85dbee83be7aac8fce87787f67085c8996@ec2-54-243-107-66.compute-1.amazonaws.com:5432/d3rdu1uade4cgo
+
+
 Route::get('/', function () {
     return view('home');
 });
